df61f5991c41ae7307ddefd3919528b85e052579
Adam Dye <31273792+adam-dye@users.noreply.github.com>
2021-05-26 14:23:51 +0100

    Removed component from test build.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   private val scalaTestPlusPlayVersion = "4.0.3"
   private val scalaTestVersion = "3.1.2"
   private val flexmarkVersion = "0.35.10"
-  private val testScope = "test,component"
+  private val testScope = "test"
 
   val customsApiCommon = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion withSources()
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % testScope
