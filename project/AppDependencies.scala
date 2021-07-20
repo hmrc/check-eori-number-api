e81e8eb60d68292f58de317a82bff17c973b1aae
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2021-07-20 17:37:58 +0100

    [IW][DLS-4372]: Play 2.8 changes. Changed AppDependencies and build.sbt to be same format has other 'check-eori... projects

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,16 @@ import play.core.PlayVersion._
 
 object AppDependencies {
 
-  private val bootstrapBackendPlayVersion = "5.3.0"
-  private val bootstrapTestPlayVersion = "4.3.0"
-  private val scalaTestPlusPlayVersion = "4.0.3"
-  private val scalaTestVersion = "3.1.2"
-  private val flexmarkVersion = "0.35.10"
-  private val testScope = "test"
+  val compile = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.7.0"
+  )
 
-  val bootstrapBackendPlay = "uk.gov.hmrc" %% "bootstrap-backend-play-27" % bootstrapBackendPlayVersion withSources()
-  val bootstrapTestPlay = "uk.gov.hmrc" %% "bootstrap-test-play-27"   % bootstrapTestPlayVersion % Test
-  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % testScope
-  val playTest = "com.typesafe.play" %% "play-test" % current % testScope
-  val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % testScope
-  val flexmark = "com.vladsch.flexmark" % "flexmark-all" % flexmarkVersion % testScope
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.7.0"                 % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
+    "com.typesafe.play"       %% "play-test"                % current                 % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"                 % Test
+  )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,9 +3,9 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 resolvers += Resolver.typesafeRepo("releases")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.12")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.13")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.9")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.8")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-settings" % "4.8.0")
 
@@ -15,7 +15,7 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "6.2.0")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.10.0-RC1")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.8.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.8.2")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 
