da4d5fa102751a3e277c82c3c4936e34bd68fb66
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2022-10-21 15:57:41 +0100

    DLS-6023 bootstrap upgrade (#21)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 import play.core.PlayVersion._
 
 object AppDependencies {
-
+  val bootstrapVersion = "7.8.0"
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.7.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.7.0" % Test,
-    "org.scalatest" %% "scalatest" % "3.2.9" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
+    "org.scalatest" %% "scalatest" % "3.2.12" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
   )
 
 }
