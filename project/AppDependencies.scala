2688b8ab16ab18cacad4cfcc53af3abc5fecaa34
Graeme Stirling <graemejstirling@gmail.com>
2023-11-22 11:52:30 +0000

    update dependencies and formatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,18 @@
 import play.core.PlayVersion
+import sbt.Keys.scalacOptions
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "7.12.0"
+  val bootstrapVersion = "7.23.0"
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion    % Test,
-    "org.scalatest"          %% "scalatest"              % "3.2.14"            % Test,
+    "org.scalatest"          %% "scalatest"              % "3.2.15"            % Test,
     "com.typesafe.play"      %% "play-test"              % PlayVersion.current % Test,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2"            % Test,
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.6"            % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"             % Test
   )
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,4 +10,3 @@ addSbtPlugin("org.scalastyle"    %% "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("uk.gov.hmrc"       %  "sbt-auto-build"        % "3.15.0")
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.0")
-
