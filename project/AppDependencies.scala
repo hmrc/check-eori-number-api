4c5ed8b6585333162c9751887dae2fd7addcd320
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2024-02-06 22:08:21 +0000

    DDCYLS-4060: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.core.PlayVersion
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "8.0.0"
+  val bootstrapVersion = "8.4.0"
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion
   )
