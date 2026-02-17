15751a5c1666351aa739e4fe95a2fdfe0fee5760
liamhenderson92 <35490828+liamhenderson92@users.noreply.github.com>
2026-02-17 16:32:14 +0000

    DDCYLS-8090: Upgrade dependencies (#57)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,12 @@ import sbt._
 object AppDependencies {
   val bootstrapVersion = "10.5.0"
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
+    "uk.gov.hmrc"         %% "bootstrap-backend-play-30" % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
+    "org.apache.commons"  %  "commons-lang3"             % "3.18.0",
+    "org.mozilla"         %  "rhino"                     % "1.8.1",
+    "ch.qos.logback"      % "logback-core"               % "1.5.27",
+    "ch.qos.logback"      % "logback-classic"            % "1.5.27",
+    "at.yawk.lz4"         %  "lz4-java"                  % "1.10.3"
   )
 
   val test: Seq[ModuleID] = Seq(
