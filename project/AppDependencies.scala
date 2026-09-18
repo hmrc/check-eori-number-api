3e4facf70944ad0c1b6c1e3a1d78e90617c73b2e
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2026-09-18 13:12:42 +0100

    fix multiple routes files using the same package warning

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,20 @@
 import play.core.PlayVersion
-import sbt._
+import sbt.*
 
 object AppDependencies {
   val bootstrapVersion = "10.8.0"
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-backend-play-30" % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
-    "org.apache.commons"  %  "commons-lang3"             % "3.18.0",
-    "org.mozilla"         %  "rhino"                     % "1.8.1",
-    "ch.qos.logback"      % "logback-core"               % "1.5.27",
-    "ch.qos.logback"      % "logback-classic"            % "1.5.27",
-    "at.yawk.lz4"         %  "lz4-java"                  % "1.10.3"
+    "org.apache.commons"  %  "commons-lang3"             % "3.20.0",
+    "org.mozilla"         %  "rhino"                     % "1.9.1",
+    "ch.qos.logback"      % "logback-core"               % "1.6.3",
+    "ch.qos.logback"      % "logback-classic"            % "1.6.3",
+    "at.yawk.lz4"         %  "lz4-java"                  % "1.11.3"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.19",
+    "org.scalatest"          %% "scalatest"              % "3.2.20",
     "org.playframework"      %% "play-test"              % PlayVersion.current,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2"
